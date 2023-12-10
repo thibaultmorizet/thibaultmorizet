6ddf230ea5e483e3499d386955e3c9c4ae212f4b
Thibaultmorizet <thibaultmorizet@people-in.fr>
2023-12-10 13:53:30 +0100

    send design

---
+++ README.md
@@ -0,0 +1,16 @@
+# My Personal Website
+
+My personal portfolio website designed to be simplistic, a dark mode toggle, an about page, and project page.
+
+## Tech Stack
+
+backend:
+- [Next.js](nextjs.org) / TypeScript
+- [MDX](https://mdxjs.com) / [Contentlayer](https://contentlayer.dev/) (Blog Posts)
+- [Prisma](https://www.prisma.io) (ORM)
+
+frontend:
+- [Tailwind CSS](https://tailwindcss.com) (Styling)
+- [Radix Primitives](https://www.radix-ui.com/primitives) (Headless UI components)
+- [Radix UI Colors](https://www.radix-ui.com/colors) (Color system)
+- [Framer Motion](https://www.framer.com/motion/) (Animations)
---
+++ app/projects/components/ui/MdxWrapper.tsx
@@ -0,0 +1,43 @@
+import { useMDXComponent } from "next-contentlayer/hooks";
+
+import CustomImage from "@/app/projects/components/ui/Image";
+import Link from "@/components/ui/Link";
+
+interface CustomLinkProps
+  extends React.DetailedHTMLProps<
+    React.AnchorHTMLAttributes<HTMLAnchorElement>,
+    HTMLAnchorElement
+  > {}
+
+const CustomLink: React.FC<CustomLinkProps> = (props) => {
+  const href = props?.href;
+  const isInternalLink = href && (href.startsWith("/") || href.startsWith("#"));
+
+  if (isInternalLink) {
+    return (
+      <Link {...props} href={href} underline>
+        {props.children}
+      </Link>
+    );
+  }
+  return (
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      className="font-normal text-link underline underline-offset-4"
+      {...props}
+    />
+  );
+};
+
+const components = {
+  Image: CustomImage,
+  a: CustomLink,
+  Link: CustomLink,
+};
+
+export default function MdxWrapper({ code }: { code: string }) {
+  const Component = useMDXComponent(code, { components });
+
+  return <Component components={components} />;
+}
